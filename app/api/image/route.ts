9cd554fda882fa8023881659bcc8eec0e226591f
JiCodes <liu.ji1@northeastern.edu>
2023-10-14 11:32:38 -0700

    fix: remove unused imports

--- app/api/image/route.ts
+++ app/api/image/route.ts
@@ -1,7 +1,6 @@
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 import OpenAI from "openai";
-import { parse } from "path";
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
