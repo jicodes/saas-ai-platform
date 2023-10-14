4d875a072fdfe10b20c86b365ee25c782be5ff8f
JiCodes <liu.ji1@northeastern.edu>
2023-10-14 10:53:05 -0700

    feat: add sign-in & sign-out page, and landing page

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,19 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+const LandingPage = () => {
+  return (
+    <div>
+      Landing page (Unprotected)
+      <div>
+        <Link href="/sign-in">
+          <Button>Login</Button>
+        </Link>
+        <Link href="/sign-up">
+          <Button>Register</Button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
