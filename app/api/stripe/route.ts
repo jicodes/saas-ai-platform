09a07ebeccda696e7f934dea1e8ef7124a798de3
JiCodes <liu.ji1@northeastern.edu>
2023-10-15 14:51:27 -0700

    feat: add user subscription schema and stripe checkout page

---
+++ app/api/stripe/route.ts
@@ -0,0 +1,69 @@
+import { auth, currentUser } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+import prismadb from "@/lib/prismadb";
+import { stripe } from "@/lib/stripe";
+import { absoluteUrl } from "@/lib/utils";
+
+const settingsUrl = absoluteUrl("/settings");
+
+export async function GET() {
+  try {
+    const { userId } = auth();
+    const user = await currentUser();
+
+    if (!userId || !user) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const userSubscription = await prismadb.userSubscription.findUnique({
+      where: {
+        userId,
+      },
+    });
+
+    if (userSubscription && userSubscription.stripeCustomerId) {
+      const stripeSession = await stripe.billingPortal.sessions.create({
+        customer: userSubscription.stripeCustomerId,
+        return_url: settingsUrl,
+      });
+      return new NextResponse(JSON.stringify({ url: stripeSession.url }));
+    }
+
+    const stripeSession = await stripe.checkout.sessions.create({
+      success_url: settingsUrl,
+      cancel_url: settingsUrl,
+      mode: "subscription",
+      payment_method_types: ["card"],
+      customer_email: user.emailAddresses[0].emailAddress,
+      billing_address_collection: "auto",
+      line_items: [
+        {
+          price_data: {
+            currency: "USD",
+            product_data: {
+              name: "Jarvis Pro Subscription",
+              description: "Unlimited AI Generations",
+            },
+            unit_amount: 2000,
+            recurring: {
+              interval: "month",
+            },
+          },
+          quantity: 1,
+        },
+      ],
+      metadata: {
+        userId,
+      },
+    });
+
+    return new NextResponse(JSON.stringify({ url: stripeSession.url }));
+
+  } catch (error) {
+    console.log("[STRIPE_ERROR]: ", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  } finally {
+    await prismadb.$disconnect();
+  }
+}
